0b5d1204980d926041ec2ace3e2cfb9ac1441751
Julien Castelain <julien.castelain@liferay.com>
2020-11-05 23:07:47 +0100

    Format .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
 	env: {
 		browser: true,
-		es6: true
+		es6: true,
 	},
 	extends: [
 		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended"
+		"plugin:@typescript-eslint/eslint-recommended",
 	],
 	globals: {
 		Atomics: "readonly",
-		SharedArrayBuffer: "readonly"
+		SharedArrayBuffer: "readonly",
 	},
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		ecmaVersion: 2018,
-		sourceType: "module"
+		sourceType: "module",
 	},
 	plugins: ["@typescript-eslint"],
-	rules: {}
+	rules: {},
 };
